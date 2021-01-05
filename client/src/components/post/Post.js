bb3d438cb23a83ce5f6571cf00041b092d66c482
Pankaj Karamchandani <pankajkaram9@gmail.com>
2021-01-05 23:03:59 +0530

    fixing post id issue

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -22,9 +22,9 @@ const Post = ({getPost, post: {post, loading}, match}) => {
             {loading ? <Spinners/> : <Fragment>
                 <Link to="/posts" className="btn">Back To Posts</Link>
                 <PostItem post={post} showActions={false}/>
-                <CommentForm postId={post._id}/>
+                <CommentForm postId={post._id || ''}/>
                 <div className="comments">
-                    {post.comments.map(comment =>(
+                    {post.comments.map(comment => (
                         <CommentItem key={comment._id} comment={comment} postId={post._id}/>
                     ))}
                 </div>
